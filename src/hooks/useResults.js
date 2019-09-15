7ba832e8c0699dc60c732519c1b57e79aad52b8d
Anna Ionova <echoespond@gmail.com>
2019-09-14 22:18:22 -0400

    Add UseEffect hook, extract hooks logic into a helper function

---
+++ src/hooks/useResults.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import yelp from '../api/yelp';
+
+
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+      // scenario if the api request fails
+    } catch (err) {
+      setErrorMessage('Something went wrong!');
+    }
+  };
+
+  // search API when component renders for the first time
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
+  return [searchApi, results, errorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,22 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-
-  const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose'
-      }
-    });
-    setResults(response.data.businesses);
-  }
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
@@ -24,7 +13,8 @@ const SearchScreen = () => {
         term={term} 
         onTermChange={setTerm} 
         onTermSubmit={searchApi} 
-    />
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
